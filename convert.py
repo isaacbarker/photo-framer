fdfb37cd4cfdedc179aa30451abde1a5a887d9ba
Isaac Barker <88422596+isaacbarker@users.noreply.github.com>
2023-08-03 17:39:21 +0100

    Added multiple image format support and text prompts when running

--- convert.py
+++ convert.py
@@ -13,9 +13,22 @@ import glob
 from PIL import Image
 
 def main():
-    # Get images from argvs
+
+    # Check correct usage
+    if (len(sys.argv) != 3):
+        return print('Incorrect usage please use: \'python(3) [input directory path] [output directory path]\'')
+
+    # Get paths
     path = sys.argv[1]
     output = sys.argv[2]
+
+    if not os.path.isdir(path):
+        return print('Please enter a valid input directory path.')
+    
+    if not os.path.isdir(output):
+        return print('Please enter a vlid output directory path.')
+
+    # Operation variables
     colour = (255, 255, 255) #rgb
     margin = 10 #px
     frame_dimension = (1080, 1350)
@@ -24,7 +37,14 @@ def main():
     if not path.endswith('/'):
         path = path + '/'
 
+    print('Finding images.')
+
+    # Add images
     filenames = glob.glob(path + '*.jpg')
+    filenames = filenames + glob.glob(path + '*.jpeg')
+    filenames = filenames + glob.glob(path + '*.png')
+
+    print(f'Found {len(filenames)} images. Framing...')
 
     # Frame each file
     i = 1
@@ -37,7 +57,6 @@ def main():
         img_aspect_ratio = img.size[1] / img.size[0]
         img_width_dimension = frame_dimension[0] - 2 * margin
         img_dimension = (int(img_width_dimension), int(img_aspect_ratio * img_width_dimension))
-        print(img_dimension)
         img_resized = img.resize(img_dimension)
 
         # Calculate img position
@@ -49,12 +68,11 @@ def main():
 
         # Save image
         output_img_path = os.path.realpath(output) + '/' + str(i) + '.jpg'
-        print(output_img_path)
         frame.save(output_img_path)
+        print(f'Successfully saved {output_img_path}...')
         i += 1
 
-        
-
+    
 if __name__ == '__main__':
     main()
 
